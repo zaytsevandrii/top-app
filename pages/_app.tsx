5b14980110bf974d30404d53943c0054877134cd
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-12-01 07:34:58 +0000

    change styles, ad eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,22 @@
-import { AppProps } from 'next/app'
-import Head from 'next/head'
-import '../styles/globals.css'
+import { AppProps } from "next/app"
+import Head from "next/head"
+import "../styles/globals.css"
 
-function MyApp({ Component, pageProps }:AppProps):JSX.Element {
-  return <>
-  <Head>
-    <title>Create Next</title>
-  </Head>
-  <Component {...pageProps} />
-  </>
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+    return (
+        <>
+            <Head>
+                <title>Create Next</title>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700;900&family=Noto+Sans+Symbols:wght@300;400;500;700;900&family=Nunito:wght@300;400;500;700;800;900&family=Open+Sans:wght@300;500;700&family=Roboto:wght@400;700&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
