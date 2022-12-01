9329734e71940fcee73a23b26d7b964b4a28d7cf
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-12-01 16:11:43 +0000

    add tag, par, buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 
-import { Button } from '../components'
-import { Htag } from '../components/Htag/Htag'
+import { Button, Htag, P } from '../components'
+
 
 export default function Home():JSX.Element {
   const a:number = 1
@@ -9,6 +9,9 @@ export default function Home():JSX.Element {
       <Htag tag='h2'>Text</Htag>
       <Button appearance='primary' arrow='right'>knopka</Button>
       <Button appearance='ghost' arrow='right'>knopka</Button>
+      <P size='l'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil quos, excepturi soluta placeat tenetur distinctio.</P>
+      <P>Srdniy</P>
+      <P size='s'>Srdniy</P>
     </div>
   )
 }
